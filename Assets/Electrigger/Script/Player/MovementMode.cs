bcd8e6b2aa2c66f051e7482a092f29a43536cafa
YutoImata <yuto.imata.wine@gmail.com>
2025-06-26 00:37:38 +0900

    WireMovementを作成中

---
+++ Assets/Electrigger/Script/Player/MovementMode.cs
@@ -0,0 +1,12 @@
+namespace Electrigger
+{
+    /// <summary>
+    /// プレイヤーの移動モードを定義する列挙型
+    /// </summary>
+    public enum MovementMode
+    {
+        Normal, // 通常移動
+        Wire    // ワイヤー移動
+    }
+    
+}
---
+++ Assets/Electrigger/Script/Player/WireMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Electrigger
+{
+    /// <summary>
+    /// ワイヤーを使用して移動を制御するクラス
+    /// </summary>
+    public class WireMovement : MonoBehaviour
+    {
+        [Header("ワイヤー設定")]
+        [SerializeField] private float wireSpeed = 15f;
+
+    }
+}
